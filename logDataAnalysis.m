bd0e86ec67a213d54f82399e586d8cb3b477f7df
Kageyana <uc.aw.ac.ce@gmail.com>
2023-02-08 20:42:56 +0900

    角速度[rad/s]に対応した

--- logDataAnalysis.m
+++ logDataAnalysis.m
@@ -2,12 +2,12 @@ clc
 clear all
 % close all
 %% パラメータ入力
-filename = 'H:/299.csv';    % 読み込みファイル名
+filename = 'H:/13.csv';    % 読み込みファイル名
 
 pulse = 60.074;	% 1mmのパルス
 step = 2;      % プロットの間隔 
-kg = 1.055;         % 補正係数
-samplestep = 16;     % 平均値のサンプル数
+kg = 1.3;         % 補正係数
+samplestep =5;     % 平均値のサンプル数
 direction = 0;  % 周回方向  0:時計回り 1:反時計回り
 gyroLSB = 16;   % ジャイロセンサの変換係数
 courseSize = [  -200    1000;
@@ -16,13 +16,15 @@ courseSize = [  -200    1000;
 log = readtable(filename);      % ログファイル読み込み
 logsize = size(log);            % 行列数取得
 pattern = log.patternTrace;     % パターン取得
-gyroZ = -log.gyroVal_Z ./10;    % z軸角速度取得
+gyroZ = -log.gyroVal_Z ./ 10000;    % z軸角速度取得[rad/s]
 Encoder = log.encCurrentN;      % 速度取得
 EncoderTotal = log.encTotalN;   % 総距離取得
 cntLog = log.cntlog;            % 時間取得
 modeCurve = log.modeCurve;      % 直線モード
 rawCurrentL = log.rawCurrentL;        % 左モータ電流値
 rawCurrentR = log.rawCurrentR;        % 左モータ電流値
+
+dt = ( cntLog(2,1) - cntLog(1,1) ) / 1000;      % サンプリング周期
 %% 電流値解析
 rawCurrent = (rawCurrentL + rawCurrentR) /2;
 rawCurrentRnomal = normalize(rawCurrentR);
@@ -41,70 +43,41 @@ velocityRmean = movmean(log.encCurrentR,samplestep);
 velocityLnomal = velocityLmean ./ max(velocityLmean);
 velocityRnomal = velocityRmean ./ max(velocityRmean);
 
-velocityL = log.encCurrentL;
-velocityR = log.encCurrentR;
-for c = 1:size(cntLog,1)
-    if c > 30
-        if rawCurrentL(c,1) - rawCurrentL(c-2,1) < 0 && velocityLnomal(c,1) - velocityLnomal(c-2,1) > 0
-            velocityL(c,1) = velocityL(c-1,1);
+velocity = Encoder ./ pulse .* 1000;                  % 速度行列[mm/s]
+velocity = movmean(velocity,samplestep);
+%% 角速解析
+angularvelocity = gyroZ .* kg;
+angularvelocitymean = movmean(angularvelocity,samplestep);
+radxy = cumtrapz(dt, angularvelocity);  % 角度行列生成
+%% 曲率半径解析
+dl = velocity .* dt;
+drad = angularvelocity .* dt;
+rcurve = dl ./ drad;
 
-        end
-        if rawCurrentR(c,1) - rawCurrent(c-2,1) < 0 && velocityRnomal(c,1) - velocityRnomal(c-2,1) > 0
-            velocityR(c,1) = velocityR(c-1,1);
-
-        end
-    end
-    
-end
-
-Encodercal = (velocityL + velocityR)/2;
-% Encoder = (velocityL + velocityR)/2;
-velocitycal = Encodercal ./ pulse .* 1000;                  % 速度行列[mm/s]
-%% 角速度解析
-for c = 1:size(cntLog,1)
-    if c > 2
-        if abs(gyroZ(c,1)) < 250
-%             gyroZ(c,1) = gyroZ(c-1,1);
-        end
-    end
-end
+% idx = modeCurve == 0;
+% rcurve(idx,:) = 0;
+idx = abs(rcurve) >2000;
+rcurve(idx,:) = 0;
+rcurve = normalize(rcurve);
 %% コース座標計算
 % 条件別行列を生成
 xc = zeros(logsize(1,1),1);
 yc = zeros(logsize(1,1),1);
 
 % 座標算出
-dt = ( cntLog(2,1) - cntLog(1,1) ) / 1000;      % サンプリング周期
 cntLog = cntLog ./ 1000;                        % [ms]から[s]に変換
-degxy = cumtrapz(cntLog, gyroZ .* kg);                % 角度行列[deg/s] 角速度を積算
-velocity = Encoder ./ pulse .* 1000;                  % 速度行列[mm/s]
-x = cumtrapz(cntLog, ( velocity .* sind(degxy) ));
-y = cumtrapz(cntLog, ( velocity .* cosd(degxy) ));
+% degxy = cumtrapz(cntLog, gyroZ .* kg);                % 角度行列[deg/s] 角速度を積算
 
-% 角度補正係数の調整
-% while 1
-%     if abs(x(end,1) - x(end-20,1)) < 1
-%         break;
-%     else
-%         kg = kg + 0.001;        % 角度補正係数
-%         degxy = cumtrapz(cntLog, gyroZ .* kg);
-%         x = cumtrapz(cntLog, ( velocity .* sind(degxy) ));
-%         y = cumtrapz(cntLog, ( velocity .* cosd(degxy) ));
-%     end
-% end
+x = cumtrapz(dt, ( velocity .* sin(radxy) ));
+y = cumtrapz(dt, ( velocity .* cos(radxy) ));
+
 txy = table(cntLog,x,y,velocity); % テーブル作成
-
-% 条件別に座標を格納する
-% 直線-カーブ変化点
-% ischange(modeCurve)
-% xc(time, 1) = x(time,1);   % x座標計算
-% yc(time, 1) = y(time,1);   % y座標計算
 %% 軌跡表示
 % パターンごとにプロット
 % colororder(newcolors)
 subplot(3,2,[1 5]);
 % scatter(xp,yp,'o','ButtonDownFcn',@(src,evnt)setXline(src,evnt,txy));
-scatter(txy,'x','y','ColorVariable','velocity');
+scatter(txy,'x','y','ColorVariable','velocity','ButtonDownFcn',@(src,evnt)setXline(src,evnt,txy));
 hold on
 scatter(txy.x(ischange(modeCurve)),txy.y(ischange(modeCurve)),'ro','ButtonDownFcn',@(src,evnt)setXline(src,evnt,txy))
 hold off
@@ -143,19 +116,19 @@ ylim([0 1.2])
 % ylabel("current[mA]")
 %% 速度表示
 subplot(3,2,4);
-plot(cntLog,[velocity velocitycal])
+plot(cntLog,velocity)
 % title("Robot velocity")
 % xlabel("time[s]")
 % ylabel("velocity[mm/s]")
 %% 角速度表示
 subplot(3,2,6);
-plot(cntLog,gyroZ)
+plot(cntLog,[rcurve log.checkMarker])
 % title("Robot angularvelocity")
 % xlabel("time[s]")
 % ylabel("angularvelocity[deg/s]")
 
-yline(250)
-yline(-250)
+yline(5)
+yline(-5)
 %% 関数
 function setXline(src,~,varargin)
     % 静的変数 
